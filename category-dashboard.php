6de2828bf6e66324532d8185abb53b554e0a7489
YanaBaljieva <yanabaljieva@gmail.com>
2022-07-05 15:45:32 +0300

    Created header.php to avoid repeating code

--- category-dashboard.php
+++ category-dashboard.php
@@ -11,34 +11,10 @@
 	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
+	<?php
+		include "header.php";
+	?>
 	<div class="site-wrapper">
-		<header class="site-header">
-			<div class="row site-header-inner">
-				<div class="site-header-branding">
-					<h1 class="site-title"><a href="index.php">Job Offers</a></h1>
-				</div>
-				<nav class="site-header-navigation">
-					<ul class="menu">
-						<li class="menu-item">
-							<a href="index.php">Home</a>					
-						</li>
-						<li class="menu-item current-menu-item">
-							<a href="dashboard.php">Dashboard</a>
-						</li>
-						<li class="menu-item">
-							<a href="profile.php">My Profile</a>					
-						</li>
-						<li class="menu-item">
-							<a href="login.php">Sign Out</a>					
-						</li>
-					</ul>
-				</nav>
-				<button class="menu-toggle">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" class='menu-toggle-bars' d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg>
-				</button>
-			</div>
-		</header>
-
 		<main class="site-main">
 			<section class="section-fullwidth section-jobs-dashboard">
 				<div class="row">						
