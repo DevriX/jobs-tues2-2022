b536eacd6e877e0ace8122c56a69829f754f630c
YanaBaljieva <yanabaljieva@gmail.com>
2022-07-07 16:47:12 +0300

    merged with master

---
+++ category_add.php
@@ -0,0 +1,32 @@
+<?php
+    $insert_category = array(
+
+		"title"   => ' ',
+
+		);
+
+		$inserts_error = array();
+
+	
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+			if(empty($_POST["title"])){
+				$inserts_error["title_err"] = "Category name field required.";
+			}
+			else{
+				$insert_category["title"] = $_POST["title"];
+			}
+
+			
+
+			if(empty($inserts_error)){
+				$sql_request = "INSERT INTO categories(title) VALUES ('".$insert_category['title']."')";
+
+				if ($con->query($sql_request) === TRUE) {
+					echo "Category created";
+				} else {
+					echo "ERROR: " . $sql_request . "<br>";
+				}
+			}
+		}
+?>
