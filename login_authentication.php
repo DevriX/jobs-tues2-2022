68317f74157f5fc3267016205be81e1d69360ab0
kgeorgiev77 <kalingeorgiev27@gmail.com>
2022-07-11 16:09:44 +0300

    Checks if emails are from devrix, strong passwords, checks if email is already used, checks if the given url for site is real and now u can upload different images with the same name

--- login_authentication.php
+++ login_authentication.php
@@ -1,10 +1,25 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if (!empty($_POST['email']) and !empty($_POST['password'])) {
+    
+    function check_hash($log_hash, $db_hash){
+        if(password_verify($log_hash, $db_hash) == true){
+            return $db_hash;
+        }
+        return 0;
+    }
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["submit"] === "login"){
+        if (!empty($_POST['email']) and !empty($_POST["password"])) {
+
+            $con1 = OpenCon();
+            $db_hash1 = "select users.password from users where email = '".$_POST['email']."' LIMIT 1"; 
+            $result1 = mysqli_query($con1, $db_hash1);  
+            $row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC);  
+            $db_pass = $row1["password"];
 
             require_once('db_connection.php');
             $con = OpenCon();
-            $sql = "select id from users where email = '".$_POST['email']."' and password = '".$_POST['password']."' LIMIT 1";  
+            $sql = "select id from users where email = '".$_POST['email']."' and password = '".check_hash($_POST["password"], $db_pass)."' LIMIT 1";  
             $result = mysqli_query($con, $sql);  
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
             $count = mysqli_num_rows($result);  
