a05880d3a04caba274456a0be07c528d1b318f40
elenavarbanova <48411601+elenavarbanova@users.noreply.github.com>
2022-07-11 16:00:52 +0300

    Single view job (#39)
    * Connection to DB
    
    * Include the config file and update the DB connection
    
    * Add the config.php in the ingored files
    
    * Fixing db connection and add file to ignored file
    
    * Change path for file in ignored file
    
    * Changes to global variables
    
    * Changes to connection to database
    
    * Display jobs from database
    
    * Display jobs from database
    
    * Small changes to reading from database
    
    * Add images to homepage to jobs
    
    * Added single view page for job and redirection from homepage
    
    * Display jobs on homepage by date
    
    * Add pagination to homepage
    
    * Fix image paths for homepage and single view page
    
    * Added path to image, fix sql selection for single job view
    
    * Remove images

--- single.php
+++ single.php
@@ -1,6 +1,7 @@
 
 	<?php
 		include 'header.php';
+		include 'related_jobs.php';
 	?>
 
 		<?php 
@@ -68,25 +69,45 @@
 					<h2 class="section-heading">Other related jobs:</h2>
 					
 					<ul class="jobs-listing">
-
+						<?php
+							$jobs = ShowRelJobs($row["id"]);
+							foreach($jobs as $row){
+								$image_path = IMAGE_PATH.$row["company_image"];
+						?>
 						<li class="job-card">
 							<div class="job-primary">
-								<h2 class="job-title"><a href="#">Front End Developer</a></h2>
+								<h2 class="job-title"><a href="single.php?id=<?php echo $row["id"]?>"><?php echo  $row["title"] ?></a></h2>
 								<div class="job-meta">
-									<a class="meta-company" href="#">Company Awesome Ltd.</a>
-									<span class="meta-date">Posted 14 days ago</span>
+									<a class="meta-company" href="single.php?id=<?php echo $row["id"]?>"><?php echo  $row["company_name"] ?></a>
+									<?php
+									if($row["Date"] == 0) {
+									?>
+									<span class="meta-date">Today</span>
+									<?php
+										} else if($row["Date"] == 1){ 
+									?>
+									<span class="meta-date">Yesterday</span>
+									<?php 
+										}else {
+											?>
+									<span class="meta-date">Posted <?php echo  $row["Date"] ?> days ago</span>
+									<?php } ?>
 								</div>
 								<div class="job-details">
-									<span class="job-location">The Hague (The Netherlands)</span>
-									<span class="job-type">Contract staff</span>
+									<span class="job-location"><?php echo  $row["company_location"] ?></span>
+									<span class="job-type"><?php echo  $row["phone_number"] ?></span>
 								</div>
 							</div>
 							<div class="job-logo">
 								<div class="job-logo-box">
-									<img src="https://i.imgur.com/ZbILm3F.png" alt="">
+									<img src=<?php echo $image_path ?> alt="">
+
 								</div>
 							</div>
 						</li>
+						<?php 
+							}
+						?>
 					</ul>
 				</div>
 			</section>
