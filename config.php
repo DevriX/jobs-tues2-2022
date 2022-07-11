b979985af8266372bf121083a5f97c612b027db1
kgeorgiev77 <kalingeorgiev27@gmail.com>
2022-07-11 16:03:06 +0300

    fix merge conflict

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ define( 'DB_USER', '' );
 define( 'DB_PASS', '' );
 define( 'DB_NAME', '' );
 define( 'DB_HOST', '' );
-define( 'IMAGE_PATH', '/uploads/company_images/');
+define( 'IMAGE_PATH', 'uploads/company_images/');
 ?>
