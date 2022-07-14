14121fd5b09e6d48ac285497f09f9989bf47be95
Elena Varbanova <elena.varbanova04@gmail.com>
2022-07-14 20:47:11 +0300

    Delete job

--- dashboard_delete.php
+++ dashboard_delete.php
@@ -5,7 +5,6 @@
      
     if(!empty($_POST['id'])){
         $id = $_POST['id'];  
-        var_dump($id);
         $sql_job = "DELETE FROM jobs WHERE jobs.id = $id";
         $sql_categories = "DELETE FROM  jobs_categories WHERE  jobs_categories.job_id = $id";
         $sql_submissions = "DELETE FROM  applications WHERE  applications.job_id = $id";
