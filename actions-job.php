9986bdf4bcfe85263bb7090eb5db86157f9cd80a
elenavarbanova <48411601+elenavarbanova@users.noreply.github.com>
2022-07-14 20:47:48 +0300

    Create edit job (#58)
    * Apply to job
    
    * Apply submission to job
    
    * Delete var_dump
    
    * Optimise getting logged user
    
    * Create job with categories
    
    * Create job
    
    * Delete job

--- actions-job.php
+++ actions-job.php
@@ -1,6 +1,73 @@
 
 	<?php
 		require_once 'header.php';
+
+		if(isset($_GET['id'])) {
+			$id = $_GET['id'];
+			$job = ShowJob($id);
+		}
+
+		$date = date('Y-m-d');
+
+		$insert_user = array(
+			"user_id"  => '',
+			"title"  => '',
+			"status"  => '',
+			"description"      => '',
+			"responsibilities"  => '',
+			"salary"  => '',
+			"date_posted"  => '',
+		);
+
+		$categories = array();
+
+		$inserts_error = array();
+
+		if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["submit"] === "create"){
+			if(empty($_POST["title"])){
+				$inserts_error["title_err"] = "Title field required.";
+			}
+			else{
+				$insert_user["title"] = ($_POST["title"]);
+			}
+
+			if(empty($_POST["description"])){
+				$inserts_error["description_err"] = "Description field required.";
+			}
+			else{
+				$insert_user["description"] = ($_POST["description"]);
+			}
+
+			if(!empty($_POST["responsibilities"])){
+				$insert_user["responsibilities"] = ($_POST["responsibilities"]);
+			}
+
+			if(!empty($_POST["salary"])){
+				$insert_user["salary"] = ($_POST["salary"]);
+			}
+
+			if(empty($inserts_error)){
+				$sql_request = "INSERT INTO jobs(user_id, title, status, description, responsibilities, salary, date_posted) VALUES ('".$user['id']."', '".$insert_user['title']."', 0, '".$insert_user['description']."', '".$insert_user['responsibilities']."', '".$insert_user['salary']."', '".$date."')";
+
+				if ($con->query($sql_request) === TRUE) {
+					echo "Job created.";
+					$last_id = mysqli_insert_id($con);
+
+					if(!empty($_POST["categories"])){
+						foreach($_POST["categories"] as $category){
+							$sql_request1 = "INSERT INTO jobs_categories(job_id, category_id) VALUES($last_id, $category)";
+							if ($con->query($sql_request1) === TRUE) {
+								echo "Category added.";
+							} else {
+								echo "ERROR: " . $sql_request1 . "<br>";
+							}
+						}
+					}
+				} else {
+					echo "ERROR: " . $sql_request . "<br>";
+				}
+			}
+		}
 	?>
 		<main class="site-main">
 			<section class="section-fullwidth">
@@ -8,26 +75,55 @@
 					<div class="flex-container centered-vertically centered-horizontally">
 						<div class="form-box box-shadow">
 							<div class="section-heading">
+							<?php
+							if(!isset($id)) {?>
 								<h2 class="heading-title">New job</h2>
+								<?php } else { ?>
+									<h2 class="heading-title">Edit job</h2>
+								<?php }?>
 							</div>
-							<form>
+							<form name="apply" action="" method="POST" enctype="multipart/form-data">
 								<div class="flex-container flex-wrap">
 									<div class="form-field-wrapper width-large">
-										<input type="text" placeholder="Job title*"/>
+										<input type="text" name="title" value="<?php if(!empty($job["title"])) { echo $job["title"]; }?>" placeholder="Job title*"/>
 									</div>
 									<div class="form-field-wrapper width-large">
-										<input type="text" placeholder="Location"/>
+										<input type="text" name="company_location" value="<?php if(!empty($job["company_location"])) { echo $job["company_location"]; }?>" placeholder="Location"/>
 									</div>
 									<div class="form-field-wrapper width-large">
-										<input type="text" placeholder="Salary"/>
+										<input type="text" name="salary" value="<?php if(!empty($job["salary"])) { echo $job["salary"]; }?>" placeholder="Salary"/>
 									</div>
 									<div class="form-field-wrapper width-large">
-										<textarea placeholder="Description*"></textarea>
+										<textarea name="description" placeholder="Description*"><?php if(!empty($job["description"])) { echo $job["description"]; }?></textarea>
 									</div>	
+									<div class="form-field-wrapper width-large">
+										<textarea name="responsibilities" placeholder="Responsibilities"><?php if(!empty($job["responsibilities"])) { echo $job["responsibilities"]; }?></textarea>
+									</div>	
+									<div class="form-field-wrapper width-large">
+										<select multiple="multiple" name="categories[]">
+										<option style="text-align:center" disabled>
+											Select one or more categories:
+										</option>
+										<?php 
+											$request = $con->query("SELECT * FROM categories");
+											if(mysqli_num_rows($request) > 0) {
+												while($row = mysqli_fetch_array($request, MYSQLI_BOTH)){
+											?>
+												<option value="<?php echo $row['id']; ?>"><?php echo $row['title']; ?></option>
+											<?php }} ?>
+										</select>
+									</div>
 								</div>
-								<button type="submit" class="button">
+								<?php
+								if(!isset($id)) {?>
+								<button type="submit" name = "submit" value="create" class="button">
 									Create
 								</button>
+								<?php } else { ?>
+									<button type="submit" name = "submit" value="update" class="button">
+									Update
+								</button>
+								<?php }?>
 							</form>
 						</div>
 					</div>
