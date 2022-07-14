c82f0cb8eb3b2747b23db34e10d949e36addbfae
kgeorgiev77 <kalingeorgiev27@gmail.com>
2022-07-14 21:24:21 +0300

    Edit job

--- actions-job.php
+++ actions-job.php
@@ -6,7 +6,7 @@
 			$id = $_GET['id'];
 			$job = ShowJob($id);
 		}
-
+		
 		$date = date('Y-m-d');
 
 		$insert_user = array(
@@ -68,6 +68,57 @@
 				}
 			}
 		}
+
+		if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["submit"] === "update"){
+			//var_dump($job["id"]);die();
+			if(empty($_POST["title"])){
+				$inserts_error["title_err"] = "Title field required.";
+			}
+			else{
+				$insert_user["title"] = ($_POST["title"]);
+			}
+
+			if(empty($_POST["description"])){
+				$inserts_error["description_err"] = "Description field required.";
+			}
+			else{
+				$insert_user["description"] = ($_POST["description"]);
+			}
+
+			if(!empty($_POST["responsibilities"])){
+				$insert_user["responsibilities"] = ($_POST["responsibilities"]);
+			}
+
+			if(!empty($_POST["salary"])){
+				$insert_user["salary"] = ($_POST["salary"]);
+			}
+
+			if(empty($inserts_error)){
+				// $sql_request = "INSERT INTO jobs(user_id, title, status, description, responsibilities, salary, date_posted) VALUES ('".$user['id']."', '".$insert_user['title']."', 0, '".$insert_user['description']."', '".$insert_user['responsibilities']."', '".$insert_user['salary']."', '".$date."')";
+				$sql_request = "UPDATE jobs SET user_id = '".$user['id']."', title =  '".$insert_user['title']."', status = 0, description =  '".$insert_user['description']."',  responsibilities =  '".$insert_user['responsibilities']."', salary = '".$insert_user['salary']."', date_posted = '".$date."'";
+				if ($con->query($sql_request) === TRUE) {
+					echo "Job Updated.";
+					$last_id = mysqli_insert_id($con);
+					$last_last_id = $id;
+					if(!empty($_POST["categories"])){
+						$sql_categories = "DELETE FROM  jobs_categories WHERE  jobs_categories.job_id = '".$id."'";
+						$res1 = $con->query($sql_categories); 
+						foreach($_POST["categories"] as $category){
+							 $sql_request1 = "INSERT INTO jobs_categories(job_id, category_id) VALUES($last_last_id, $category)";
+							if ($con->query($sql_request1) === TRUE) {
+								echo "Category updated.";
+							} else {
+								echo "ERROR: " . $sql_request1 . "<br>";
+							}
+						}
+					}
+				} else {
+					echo "ERROR: " . $sql_request . "<br>";
+				}
+			}
+		}
+
+
 	?>
 		<main class="site-main">
 			<section class="section-fullwidth">
