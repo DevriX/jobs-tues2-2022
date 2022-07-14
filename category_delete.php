deec41374046c38f7d0bda827bdf79d5e45bf7ac
YanaBaljieva <yanabaljieva@gmail.com>
2022-07-14 12:54:59 +0300

    merge conflict fix

--- category_delete.php
+++ category_delete.php
@@ -2,8 +2,9 @@
     if($_SERVER["REQUEST_METHOD"] == "GET"){
         
         if(!empty($_GET['cat_id'])){
-            $id = $_GET['cat_id'];  
-            $sql = "DELETE FROM categories WHERE id=".$id;
+            $id  = $_GET['cat_id'];  
+            $sql = "DELETE FROM categories 
+                    WHERE id=".$id;
             $con->query($sql);
             
         }
